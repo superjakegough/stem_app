97c78f43a6358e5bf06a4d4283980b79fad47b9b
jacobgoughamplyfi <jacob.gough@amplyfigroup.com>
2019-07-02 16:17:26 +0100

    syntax changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-//const Data = require('./data');
+const Data = require('./data');
 
 const API_PORT = 3001;
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 const router = express.Router();
 
 // this is our MongoDB database
-const dbRoute = 'mongodb://localhost/my_database';
+const dbRoute = 'mongodb://localhost/stem_database';
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });

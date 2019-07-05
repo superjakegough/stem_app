45c33a998524a9b8db70548749b5f933dea593b5
SnakeyHips <super.jake@hotmail.co.uk>
2019-07-05 20:40:43 +0100

    checkpoint

--- frontend/src/components/home/home.vue
+++ frontend/src/components/home/home.vue
@@ -13,11 +13,15 @@
          <v-layout row wrap justify-space-between>
             <v-flex ref="whoBox" md8 xs12 mb-4>
                <h2 class="content-title primary--text mb-4">Who We Are</h2>
-               <p>We are Stem. We are not just here to get a fee - we are here to grow your business and grow your career.</p>
-               <p>We are more than just a specialist recruitment consultancy; we also aim to support the growth of the industries which we recruit for in mutual benefit.</p>
-               <v-layout justify-center>
-                  <v-btn flat color="primary" to="/biography">Biography</v-btn>
-               </v-layout>
+               <p>
+                  We are Stem. We are not just here to get a fee – we are here to <strong>grow your business</strong> and <strong>grow your career</strong>. We are more than just a specialist recruitment consultancy; we also aim to support the growth of the industries for which we recruit, in mutual benefit.
+               </p>
+               <p>
+                  Based in South Wales, we provide expertise and experience in recruitment for the science, technology, engineering and manufacturing industries across the region. Our approach to recruitment is simple and transparent, with a focus on not just short-term solutions to find you the right candidate, but also on investing into long-term solutions for your recruitment needs.
+               </p>
+               <p>
+                  Whether you are a business looking to recruit, or a candidate looking to take the next step in your career, then we are here to listen to your needs and expectations, offer market advice and knowledge, and work with you to find you the right match.
+               </p>
             </v-flex>
             <v-flex xs3 ml-3 v-if="$vuetify.breakpoint.mdAndUp" mt-4 mb-4>
                <v-layout fill-height>
@@ -34,11 +38,12 @@
                <v-expansion-panel v-model="clientPanel" expand popout>
                   <v-expansion-panel-content  v-bind:style="{'background-color': getBackground(clientPanel[0])}">
                      <template v-slot:header>
-                        <div>Clients</div>
+                        <div class="body-2">Clients</div>
                      </template>
                      <v-card>
                         <v-card-text>
-                          Stem supports science, technology, engineering and manufacturing businesses in South Wales by providing permanent recruitment services. We provide experience and expertise in recruiting for a wide range of roles and are dedicated to finding the right candidates to help your business grow. Take a look at our client services page to find out more information, or if you have a vacancy and need help, then email vacancy@stemrecruit.co.uk.
+                          Stem supports science, technology, engineering and manufacturing businesses in South Wales by providing permanent recruitment services. We provide experience and expertise in recruiting for a wide range of roles and are dedicated to finding the right candidates to help your business grow. Take a look at our client services page to find out more information, or if you have a vacancy and need help, then email 
+                          <a href="mailto:vacancy@stemrecruit.co.uk">vacancy@stemrecruit.co.uk.</a>
                         </v-card-text>
                         <v-card-actions>
                           <v-spacer></v-spacer>
@@ -51,11 +56,11 @@
                <v-expansion-panel v-model="candidatePanel" expand popout>
                   <v-expansion-panel-content v-bind:style="{'background-color': getBackground(candidatePanel[0])}">
                      <template v-slot:header>
-                        <div>Candidates</div>
+                        <div class="body-2">Candidates</div>
                      </template>
                      <v-card>
                         <v-card-text>
-                          If you’re looking to take the next step in your career, then we can help. All of our services to candidates may be free, but we take the same time and care in finding YOU the right fit as we do for our clients. To see our current job opportunities with our clients, take a look at our find a job page, or alternatively you can register with us by sending your CV to jobs@stemrecruit.co.uk and we will be in contact as soon as we have any suitable job opportunities.
+                          If you’re looking to take the next step in your career, then we can help. All of our services to candidates may be free, but we take the same time and care in finding YOU the right fit as we do for our clients. To see our current job opportunities with our clients, take a look at our find a job page, or alternatively you can register with us by sending your CV to <a href="mailto:jobs@stemrecruit.co.uk">jobs@stemrecruit.co.uk.</a> and we will be in contact as soon as we have any suitable job opportunities.
                         </v-card-text>
                         <v-card-actions>
                           <v-spacer></v-spacer>
@@ -66,47 +71,22 @@
                   </v-expansion-panel-content>
                </v-expansion-panel>
             </v-flex>
-            <v-flex md3 xs12 text-xs-center>
-               <v-avatar size="200" class="mb-4">
-                  <v-img :src="require('@/assets/review1.jpg')"></v-img>
-               </v-avatar>
-               <h2 class="primary--text title font-weight-light text-uppercase mb-3">Joe Blogss</h2>
-               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-            </v-flex>
-            <v-flex md3 xs12 text-xs-center>
-               <v-avatar size="200" class="mb-4">
-                  <v-img :src="require('@/assets/review2.jpg')"></v-img>
-               </v-avatar>
-               <h2 class="primary--text title font-weight-light text-uppercase mb-3">Jane Doe</h2>
-               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "</p>
-            </v-flex>
-            <v-flex md3 xs12 text-xs-center>
-               <v-avatar size="200" class="mb-4">
-                  <v-img :src="require('@/assets/review3.jpg')"></v-img>
-               </v-avatar>
-               <h2 class="primary--text title font-weight-light text-uppercase mb-3">John Hancock</h2>
-               <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."</p>
-            </v-flex>
             <v-flex xs12 ma-4>
                <v-divider></v-divider>
             </v-flex>
-            <v-flex md8 xs12>
-               <h2 class="content-title primary--text mb-4">Contact Us</h2>
-               <h2 class="primary--text title font-weight-light text-uppercase mb-3">Send me a message</h2>
-               <v-form ref="form" lazy-validation>
-                  <v-text-field v-model="name" label="Name" box required></v-text-field>
-                  <v-text-field v-model="email" label="Email" box required></v-text-field>
-                  <v-text-field v-model="subject" label="Subject" box required></v-text-field>
-                  <v-text-field v-model="message" label="Message" height="200px" box required></v-text-field>
-                  <v-layout v-if="$vuetify.breakpoint.mdAndUp" justify-end>
-                     <v-btn raised color="primary" class="ma-0">Contact Me</v-btn>
-                  </v-layout>
-                  <v-layout v-else justify-center>
-                     <v-btn raised color="primary" >Contact Me</v-btn>
-                  </v-layout>
-               </v-form>
+            <v-flex md4 xs12 text-xs-center>
+               <v-avatar size="200" class="mb-4">
+                  <v-img :src="require('@/assets/jack.jpg')"></v-img>
+               </v-avatar>
+               <h2 class="primary--text title text-uppercase mb-3">Jack Taylor</h2>
+               <h3 class="primary--text subtitle text-uppercase mb-3">Founder &amp; Recruitment Advisor</h3>
             </v-flex>
-            <v-flex md3 xs12 mb-5>
+            <v-flex md4 xs12 align-center fill-height>
+               <p>
+                  I am a personable and technical recruiter with a simple and transparent approach that has both client and candidate satisfaction at its core. I offer extensive market knowledge of the science, technology, engineering and manufacturing industries in South Wales, along with a large network of candidates and clients. I have recruited for a variety of companies; from start-ups and SMEs, right through to blue-chip organisations and global brands.
+               </p>
+            </v-flex>
+            <v-flex md4 xs12 mb-5>
                <v-layout align-center justify-center fill-height>
                   <v-list style="background: transparent;">
                      <v-list-tile avatar three-line>
@@ -151,6 +131,22 @@
                   </v-list>
                </v-layout>
             </v-flex>
+            <!-- <v-flex md8 xs12>
+               <h2 class="content-title primary--text mb-4">Contact Us</h2>
+               <h2 class="primary--text title font-weight-light text-uppercase mb-3">Send me a message</h2>
+               <v-form ref="form" lazy-validation>
+                  <v-text-field v-model="name" label="Name" box required></v-text-field>
+                  <v-text-field v-model="email" label="Email" box required></v-text-field>
+                  <v-text-field v-model="subject" label="Subject" box required></v-text-field>
+                  <v-text-field v-model="message" label="Message" height="200px" box required></v-text-field>
+                  <v-layout v-if="$vuetify.breakpoint.mdAndUp" justify-end>
+                     <v-btn raised color="primary" class="ma-0">Contact Me</v-btn>
+                  </v-layout>
+                  <v-layout v-else justify-center>
+                     <v-btn raised color="primary" >Contact Me</v-btn>
+                  </v-layout>
+               </v-form>
+            </v-flex> -->
          </v-layout>
       </v-container>
    </v-layout>

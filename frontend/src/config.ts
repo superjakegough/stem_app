4f9147da8248cdfacc985ae551e6569dc8698f10
Jake <super.jake@hotmail.co.uk>
2019-08-08 21:46:08 +0100

    added aws config to front end

---
+++ frontend/src/config.ts
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-1",
+    BUCKET: "stem-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-west-1",
+    URL: "https://osrjfu4hqf.execute-api.eu-west-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-1",
+    USER_POOL_ID: "eu-west-1_uJ1hdOLrj",
+    APP_CLIENT_ID: "4bbfbmkidksu3okmln8vehah44",
+    IDENTITY_POOL_ID: "eu-west-1:49ee8954-4814-4011-b2f7-8872ae1a2ca1"
+  }
+};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import Vue from "vue";
 import "@/plugins/vuetify";
 import App from "@/components/app/app.vue";
 import router from "@/router";
+import Amplify from "aws-amplify";
+import config from "./config";
 
 Vue.config.productionTip = false;
 
@@ -9,3 +11,33 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "jobs",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+      {
+        name: "blogs",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      }
+    ]
+  }
+});
+

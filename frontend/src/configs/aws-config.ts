45944941d788bae08d9e403662f1a8f33b24bc10
Jake <super.jake@hotmail.co.uk>
2019-08-09 14:03:10 +0100

    updated values

--- frontend/src/configs/aws-config.ts
+++ frontend/src/configs/aws-config.ts
@@ -5,12 +5,12 @@ export default {
   },
   apiGateway: {
     REGION: "eu-west-1",
-    URL: "https://b7cph5flfb.execute-api.eu-west-1.amazonaws.com/dev"
+    URL: "https://osrjfu4hqf.execute-api.eu-west-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "eu-west-1",
-    USER_POOL_ID: "eu-west-1_afxjTOzXu",
-    APP_CLIENT_ID: "324ic60d2ta76hoh3jc590s2sk",
-    IDENTITY_POOL_ID: "eu-west-1:5dcb756a-9218-4092-b71d-d74a837f6794"
+    USER_POOL_ID: "eu-west-1_7S1DThBbz",
+    APP_CLIENT_ID: "5uj4gki5457en6s5us70ab67ni",
+    IDENTITY_POOL_ID: "eu-west-1:5f7a0401-1c4c-4f1d-a619-5ae99020d88d"
   }
 };

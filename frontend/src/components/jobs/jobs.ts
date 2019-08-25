2c3dd117c26c6f256443270a3318941f7b525c33
Jake <super.jake@hotmail.co.uk>
2019-08-25 17:21:30 +0100

    job search checkpoint

--- frontend/src/components/jobs/jobs.ts
+++ frontend/src/components/jobs/jobs.ts
@@ -13,14 +13,38 @@ export default class JobsComponent extends Vue {
   jobsPages: number = 0;
   jobsPaged: Job[] = [];
   searchTerm: string = "";
+  searchDialog: boolean = false;
+  job: Job = {
+    jobId: "",
+    title: "",
+    salary: "",
+    benefits: "",
+    jobType: "",
+    jobLocation: "",
+    jobReference: "",
+    description: "",
+    jobFilled: "",
+    createdAt: ""
+  };
+  salaries: string[] = [
+    "> £20,000",
+    "£20,000+",
+    "£30,000+",
+    "£40,000+",
+    "£50,000+",
+  ];
+  benefits: string[] = [];
+  jobTypes: string[] = [];
+  jobLocations: string[] = [];
 
   async mounted() {
     this.loading = true;
     const res = await getAllJobs();
     if (res) {
       this.jobs = res;
+      this.populateSets();
+      this.onSearch();
     }
-    this.onSearch();
     this.loading = false;
   }
 
@@ -30,16 +54,40 @@ export default class JobsComponent extends Vue {
   }
 
   onSearch() {
-    const regex = new RegExp(`^.*${this.searchTerm}.*$`, "i");
+    const regex = new RegExp(`^.*${this.job.title}.*$`, "i");
     this.page = 1;
-    if (!this.searchTerm) {
+    this.searchTerm = `${this.job.title}, ${this.job.salary}, ${this.job.benefits}, ${this.job.jobType}, ${this.job.jobLocation}`;
+    if (!this.searchTerm.replace(", ", "")) {
       this.filteredJobs = this.jobs;
     } else {
-      this.filteredJobs = this.jobs.filter(job => regex.test(job.title));
+      this.filteredJobs = this.jobs.filter(job => this.checkJob(job));
     }
     this.onPageChange();
   }
 
+  populateSets() {
+    const benefitsSet: Set<string> = new Set<string>();
+    const jobTypesSet: Set<string> = new Set<string>();
+    const jobLocationsSet: Set<string> = new Set<string>();
+
+    for (let i = 0; i < this.jobs.length; i++) {
+      benefitsSet.add(this.jobs[i].benefits);
+      jobTypesSet.add(this.jobs[i].jobType);
+      jobLocationsSet.add(this.jobs[i].jobLocation);
+    }
+    this.benefits = [...benefitsSet];
+    this.jobTypes = [...jobTypesSet];
+    this.jobLocations = [...jobLocationsSet];
+  }
+
+  checkJob(checkJob: Job) {
+    const regex = new RegExp(`^.*${this.job.title}.*$`, "i");
+    if (this.job.title) {
+      if (regex.test(checkJob.title)) {
+        
+      }
+    }
+  }
 
   openRoute(id: string) {
     this.$router.push({ path: `/job/${id}` });

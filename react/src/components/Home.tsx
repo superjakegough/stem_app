04e45126471ff70d45c3b05a1b0716933c107b5f
Jake <super.jake@hotmail.co.uk>
2019-08-28 10:38:47 +0100

    home done

--- react/src/components/Home.tsx
+++ react/src/components/Home.tsx
@@ -8,17 +8,26 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Divider from "@material-ui/core/Divider";
-import Avatar from "@material-ui/core/Avatar"; 
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import EmailIcon from "@material-ui/icons/Email";
+import PhoneIcon from "@material-ui/icons/Phone";
 import homeimage from "../assets/home.jpg";
 import stemlogo from "../assets/stem_green.png";
 import avatar from "../assets/jack.jpg";
+import rec from "../assets/rec.jpg";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   avatar: {
     height: 200,
     width: 200,
+    margin: "auto",
     marginBottom: theme.spacing(4)
   },
   bodyText: {
@@ -33,6 +42,13 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
   },
   expansionSummary: {
     fontWeight: 500
+  },
+  fillHeight: {
+    height: "100%"
+  },
+  recLogo: {
+    height: 100,
+    margin: "auto"
   }
 }));
 
@@ -122,8 +138,51 @@ const Home: React.FunctionComponent = props => {
           <Grid item sm={10} xs={12}>
             <Divider className={classes.divider}/>
           </Grid>
-          <Grid item md={4} sm={10} xs={12} className="mt-24 text-center">
+          <Grid item md={4} sm={10} xs={12} className="mt-24 mb-24 text-center">
             <Avatar src={avatar} alt="" className={classes.avatar} />
+            <Typography variant="h6" className="mb-16">
+              JACK TAYLOR
+            </Typography>
+            <Typography variant="h6" className="mb-16">
+              Founder &amp; Recruitment Consultant
+            </Typography>
+          </Grid>
+          <Grid item md={4} sm={10} xs={12}>
+            <Grid container justify="center" alignItems="center" className={classes.fillHeight}>
+              <Typography>
+                I am a personable and technical recruiter with a simple and transparent approach that has both client and candidate satisfaction at its core. I offer extensive market knowledge of the science, technology, engineering and manufacturing industries in Wales, along with a large network of candidates and clients. I have recruited for a variety of companies; from start-ups and SMEs, right through to blue-chip organisations and global brands.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md={4} sm={10} xs={12} className="mb-24">
+            <Grid container justify="center" alignItems="center" className={classes.fillHeight}>
+              <List>
+                <ListItem>
+                  <ListItemIcon>
+                    <LocationOnIcon color="primary" fontSize="large"/>
+                  </ListItemIcon>
+                  <ListItemText primary="Location" secondary="Welsh ICE, Caerphilly, CF83 3GG"/>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon >
+                    <EmailIcon color="primary" fontSize="large"/>
+                  </ListItemIcon>
+                  <ListItemText primary="Email" secondary="jack.taylor@stemrecruit.co.uk"/>
+                </ListItem>
+                <ListItem>
+                  <ListItemIcon>
+                    <PhoneIcon color="primary" fontSize="large"/>
+                  </ListItemIcon>
+                  <ListItemText primary="Telephone" secondary="029 2120 2879"/>
+                </ListItem>
+                <ListItem>
+                  <img src={rec} className={classes.recLogo} />
+                </ListItem>
+              </List>
+            </Grid>
+          </Grid>
+          <Grid container justify="center" className="mb-32">
+            <Button className={classes.button} color="primary" component={Link} to="/privacy">Privacy Notice</Button>
           </Grid>
         </Grid>
       </Grid>

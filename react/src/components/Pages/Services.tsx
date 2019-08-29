b512afc5955010135068d16ed1aa6241acadc587
Jake <super.jake@hotmail.co.uk>
2019-08-29 09:48:38 +0100

    typography services convert

--- react/src/components/Pages/Services.tsx
+++ react/src/components/Pages/Services.tsx
@@ -14,9 +14,6 @@ import Paper from "@material-ui/core/Paper";
 import servicesimage from "../../assets/services.jpg";
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-  bodyText: {
-    marginBottom: theme.spacing(1)
-  },
   button: {
     marginTop: theme.spacing(1),
     marginRight: theme.spacing(1),
@@ -55,61 +52,59 @@ const Services: React.FunctionComponent = props => {
       case 0:
         return (
           <>
-            <Typography className={classes.bodyText}>Via a telephone or face-to-face meeting, we listen to your recruitment needs and:</Typography>
-            <List className="mb-24">
-              <ListItem><ListItemText>&bull; Discuss the job description and understand the job responsibilities.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Understand the skills and qualifications that you are looking for, identifying essential and desirable requirements.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Understand the training, progression and other opportunities the vacancy might hold.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Understand your business culture and establish behavioral competencies that will be needed to fit in with the role and company culture.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; We can provide expert advice as to what skills are available, as well as advice on industry trends.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Once we have established what you are looking for, we tailor our recruitment process to meet your requirements, and begin a candidate sourcing and selection process.</ListItemText></ListItem>
-            </List>
+            <p>Via a telephone or face-to-face meeting, we listen to your recruitment needs and:</p>
+            <ul className="mb-24">
+              <li> Discuss the job description and understand the job responsibilities.</li>
+              <li> Understand the skills and qualifications that you are looking for, identifying essential and desirable requirements.</li>
+              <li> Understand the training, progression and other opportunities the vacancy might hold.</li>
+              <li> Understand your business culture and establish behavioral competencies that will be needed to fit in with the role and company culture.</li>
+              <li> We can provide expert advice as to what skills are available, as well as advice on industry trends.</li>
+              <li> Once we have established what you are looking for, we tailor our recruitment process to meet your requirements, and begin a candidate sourcing and selection process.</li>
+            </ul>
           </>
         );
       case 1:
           return (
             <>
-            <Typography className={classes.bodyText}>We search through our large network of candidates to find you the right candidate. We use a variety of sourcing tools including:</Typography>
-            <List className="mb-24">
-              <ListItem><ListItemText>&bull; Internal database of science, technology, engineering and manufacturing professionals in Wales.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; UK job boards – including advertising and access to millions of CVs UK-wide.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Social media – LinkedIn, Twitter and Facebook.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Stem Website – advertising on Stem’s website job page.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Networking and events – we network at industry events meeting with both active and passive candidates.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Education links – for graduate roles, we have strong links with Wales’ universities.</ListItemText></ListItem>
-            </List>
+              <p>We search through our large network of candidates to find you the right candidate. We use a variety of sourcing tools including:</p>
+              <ul className="mb-24">
+                <li> Internal database of science, technology, engineering and manufacturing professionals in Wales.</li>
+                <li> UK job boards – including advertising and access to millions of CVs UK-wide.</li>
+                <li> Social media – LinkedIn, Twitter and Facebook.</li>
+                <li> Stem Website – advertising on Stem’s website job page.</li>
+                <li> Networking and events – we network at industry events meeting with both active and passive candidates.</li>
+                <li> Education links – for graduate roles, we have strong links with Wales’ universities.</li>
+              </ul>
             </>
           );
       case 2:
         return (
           <>
-          <Typography className={classes.bodyText}>Candidates selected from sourcing will be screened further via a telephone or face-to-face interview by Stem and will be evaluated on the following:</Typography>
-            <List className="mb-24">
-              <ListItem><ListItemText>&bull; Current salary, benefits package and expectations vs. what your opportunity offers.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Location/commute to your business and hours of work.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Relevant skills, qualifications and behavioral competencies.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Career plans and aspirations.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Availability.</ListItemText></ListItem>
-            </List>
+            <p>Candidates selected from sourcing will be screened further via a telephone or face-to-face interview by Stem and will be evaluated on the following:</p>
+            <ul className="mb-24">
+              <li> Current salary, benefits package and expectations vs. what your opportunity offers.</li>
+              <li> Location/commute to your business and hours of work.</li>
+              <li> Relevant skills, qualifications and behavioral competencies.</li>
+              <li> Career plans and aspirations.</li>
+              <li> Availability.</li>
+            </ul>
           </>
         );
       case 3:
         return (
           <>
-          <Typography className={classes.bodyText}>Following the first stage Stem interview, candidates will be sent a job application email including:</Typography>
-            <List className="mb-24">
-              <ListItem><ListItemText>&bull; The job description.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Job advert.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Your company website.</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; An application form with questions or technical test can also be sent. The job application email tests the candidate’s interest and commitment.</ListItemText></ListItem>
-            </List>
+            <p>Following the first stage Stem interview, candidates will be sent a job application email including:</p>
+            <ul className="mb-24">
+              <li> The job description.</li>
+              <li> Job advert.</li>
+              <li> Your company website.</li>
+              <li> An application form with questions or technical test can also be sent. The job application email tests the candidate’s interest and commitment.</li>
+            </ul>
           </>
         );
       case 4:
         return (
-          <>
-            <Typography className={classes.bodyText}>Once the candidate has reviewed the job application email, Stem will conduct a final interview with the candidate to further gauge their interest and suitability for the role before presenting you their details. Once we have sent you their CV, we will continue to provide support during the process from when you first interview the candidate, through to the job offer and the candidate’s first day, providing feedback and advice to you and the candidate throughout.</Typography>
-          </>
+          <p>Once the candidate has reviewed the job application email, Stem will conduct a final interview with the candidate to further gauge their interest and suitability for the role before presenting you their details. Once we have sent you their CV, we will continue to provide support during the process from when you first interview the candidate, through to the job offer and the candidate’s first day, providing feedback and advice to you and the candidate throughout.</p>
         );
     }
   }
@@ -124,9 +119,9 @@ const Services: React.FunctionComponent = props => {
         <Grid container justify="center" className="content-container">
           <Grid item md={8} sm={10} xs={12} className="mb-24">
             <h2 className="content-title mb-24">Client Services</h2>
-            <Typography className={classes.bodyText}>Stem is a specialist permanent recruitment consultancy providing sustainable recruitment solutions for the science, technology, engineering and manufacturing industries in Wales. Our approach to recruitment is simple but that of care, integrity, attention to detail, and solutions that make a genuine difference, short-term and long-term.</Typography>
+            <p>Stem is a specialist permanent recruitment consultancy providing sustainable recruitment solutions for the science, technology, engineering and manufacturing industries in Wales. Our approach to recruitment is simple but that of care, integrity, attention to detail, and solutions that make a genuine difference, short-term and long-term.</p>
             <h2 className="content-title mb-24">Our Recruitment Process</h2>
-            <Typography className={classes.bodyText}>We provide a thorough 5-stage recruitment and vetting process which ensures that we not only find the right fit for our clients, but also the right fit for the candidate, ensuring all parties are satisfied.</Typography>
+            <p>We provide a thorough 5-stage recruitment and vetting process which ensures that we not only find the right fit for our clients, but also the right fit for the candidate, ensuring all parties are satisfied.</p>
             <Stepper activeStep={activeStep} orientation="vertical">
               {steps.map((label, index) => (
                 <Step key={label}>
@@ -166,26 +161,26 @@ const Services: React.FunctionComponent = props => {
           </Grid>
           <Grid item md={8} sm={10} xs={12} className="mb-24">
             <h2 className="content-title mt-24 mb-24">Industries</h2>
-            <Typography className={classes.bodyText}>We have expertise and experience in recruiting for the following industries:</Typography>
-            <List className="mb-24">
-              <ListItem><ListItemText>&bull; Automotive</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Aerospace</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Pharmaceutical</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Life Science</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Medical Device</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Electronic</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Semiconductor</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; FMCG</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Food Manufacturing</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Oil &amp; Gas</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Chemicals Manufacturing</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Materials Manufacturing</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Heavy Industry</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Digital</ListItemText></ListItem>
-              <ListItem><ListItemText>&bull; Technology</ListItemText></ListItem>
-            </List>
-            <Typography className={classes.bodyText}>If you would like to find out more about how Stem can help grow your business, or if you have a vacancy and need help, then call us on 029 2120 2879, or email {" "}
-            <a href="mailto:info@stemrecruit.co.uk">info@stemrecruit.co.uk.</a></Typography>
+            <p>We have expertise and experience in recruiting for the following industries:</p>
+            <ul className="mb-24">
+            <li> Automotive</li>
+              <li> Aerospace</li>
+              <li> Pharmaceutical</li>
+              <li> Life Science</li>
+              <li> Medical Device</li>
+              <li> Electronic</li>
+              <li> Semiconductor</li>
+              <li> FMCG</li>
+              <li> Food Manufacturing</li>
+              <li> Oil &amp; Gas</li>
+              <li> Chemicals Manufacturing</li>
+              <li> Materials Manufacturing</li>
+              <li> Heavy Industry</li>
+              <li> Digital</li>
+              <li> Technology</li>
+            </ul>
+            <p>If you would like to find out more about how Stem can help grow your business, or if you have a vacancy and need help, then call us on 029 2120 2879, or email {" "}
+            <a href="mailto:info@stemrecruit.co.uk">info@stemrecruit.co.uk.</a></p>
           </Grid>
         </Grid>
       </Grid>

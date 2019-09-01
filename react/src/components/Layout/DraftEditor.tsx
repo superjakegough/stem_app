9ccf61759569b73a37e089469ed434b2bd240ae3
Jake <super.jake@hotmail.co.uk>
2019-09-01 13:39:43 +0100

    draft js checkpoint

--- react/src/components/Layout/DraftEditor.tsx
+++ react/src/components/Layout/DraftEditor.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import FormatBoldIcon from "@material-ui/icons/FormatBold";
+import FormatItalicIcon from "@material-ui/icons/FormatItalic";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import FormatListNumberedIcon from "@material-ui/icons/FormatListNumbered";
+import InsertLinkIcon from "@material-ui/icons/InsertLink";
+import InsertPhotoIcon from "@material-ui/icons/InsertPhoto";
 import { Editor, EditorState } from "draft-js";
 import { stateToHTML } from "draft-js-export-html";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    divider: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(2)
+    },
     editor: {
       backgroundColor: "#E8E8E8",
       borderRadius: 8,
       padding: theme.spacing(2),
       maxHeight: 500,
       overflowY: "scroll"
+    },
+    icon: {
+      marginRight: theme.spacing(1)
     }
   })
 );
@@ -37,6 +52,27 @@ const DraftEditor: React.FunctionComponent<DraftEditorProps> = props => {
 
   return (
     <div onClick={focusEditor} className={classes.editor}>
+      <div>
+        <IconButton size="small" className={classes.icon}>
+          <FormatBoldIcon />
+        </IconButton>
+        <IconButton size="small" className={classes.icon}>
+          <FormatItalicIcon />
+        </IconButton>
+        <IconButton size="small" className={classes.icon}>
+          <FormatListBulletedIcon />
+        </IconButton>
+        <IconButton size="small" className={classes.icon}>
+          <FormatListNumberedIcon />
+        </IconButton>
+        <IconButton size="small" className={classes.icon}>
+          <InsertLinkIcon />
+        </IconButton>
+        <IconButton size="small" className={classes.icon}>
+          <InsertPhotoIcon />
+        </IconButton>
+      </div>
+      <Divider className={classes.divider} />
       <Editor
         ref={editor}
         editorState={editorState}

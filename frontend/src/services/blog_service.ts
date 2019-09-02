235bdd6745660efc2ac2a74f0014cea9ff584507
Jake <super.jake@hotmail.co.uk>
2019-09-02 14:44:12 +0100

    rename react directory to frontend

--- frontend/src/services/blog_service.ts
+++ frontend/src/services/blog_service.ts
@@ -1,29 +1,29 @@
-import Blog from "../models/blog";
+import { Blog } from "../models/blog";
 import API from "@aws-amplify/api";
 
 const apiName: string = "blogs";
 const apiPath: string = "/blogs";
 
-export const getAllBlogs = async () => {
+export const GetAllBlogs = async (): Promise<Blog[]> => {
   return API.get(apiName, apiPath, {});
 };
 
-export const getBlog = async (id: string) => {
+export const GetBlog = async (id: string): Promise<Blog> => {
   return API.get(apiName, `${apiPath}/${id}`, {});
 };
 
-export const createBlog = async (blog: Blog) => {
+export const CreateBlog = async (blog: Blog): Promise<any> => {
   return API.post(apiName, apiPath, {
     body: blog
   });
 };
 
-export const updateBlog = async (blog: Blog) => {
+export const UpdateBlog = async (blog: Blog): Promise<any> => {
   return API.put(apiName, `${apiPath}/${blog.blogId}`, {
     body: blog
   });
 };
 
-export const deleteBlog = async (id: string) => {
+export const DeleteBlog = async (id: string): Promise<any> => {
   return API.del(apiName, `${apiPath}/${id}`, {});
 };
--- frontend/src/services/job_service.ts
+++ frontend/src/services/job_service.ts
@@ -4,26 +4,26 @@ import API from "@aws-amplify/api";
 const apiName: string = "jobs";
 const apiPath: string = "/jobs";
 
-export const getAllJobs = async () => {
+export const GetAllJobs = async (): Promise<Job[]> => {
   return API.get(apiName, apiPath, {});
 };
 
-export const getJob = async (id: string) => {
+export const GetJob = async (id: string): Promise<Job> => {
   return API.get(apiName, `${apiPath}/${id}`, {});
 };
 
-export const createJob = async (job: Job) => {
+export const CreateJob = async (job: Job): Promise<any> => {
   return API.post(apiName, apiPath, {
     body: job
   });
 };
 
-export const updateJob = async (job: Job) => {
+export const UpdateJob = async (job: Job): Promise<any> => {
   return API.put(apiName, `${apiPath}/${job.jobId}`, {
     body: job
   });
 };
 
-export const deleteJob = async (id: string) => {
+export const DeleteJob = async (id: string): Promise<any> => {
   return API.del(apiName, `${apiPath}/${id}`, {});
 };

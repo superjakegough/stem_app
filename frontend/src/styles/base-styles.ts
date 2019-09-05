12b0e7f79d420b444b448c18d4b78400bd249634
Jake <super.jake@hotmail.co.uk>
2019-09-05 09:30:22 +0100

    styles checkpoint

--- frontend/src/styles/base-styles.ts
+++ frontend/src/styles/base-styles.ts
@@ -1,38 +1,89 @@
-import { makeStyles } from "@material-ui/styles";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 
-export const useStylesBase = makeStyles({
-  main: {
-    fontFamily: "Roboto, Helvetica, Arial, sans-serif",
-    "& a": {
-      color: "#22b771",
-      textDecoration: "unset",
-      wordBreak: "break-word"
+export const useStylesBase = makeStyles((theme: Theme) =>
+  createStyles({
+    main: {
+      fontFamily: "Roboto, Helvetica, Arial, sans-serif",
+      "& a": {
+        color: theme.palette.primary.main,
+        textDecoration: "unset",
+        wordBreak: "break-word"
+      },
+      "& p": {
+        fontSize: "1rem",
+        fontWeight: 400,
+        lineHeight: 1.5,
+        letterSpacing: "0.00938em"
+      },
+      "& li": {
+        fontSize: "1rem",
+        fontWeight: 400,
+        lineHeight: 1.5,
+        letterSpacing: "0.00938em"
+      },
+      "& h6": {
+        fontSize: "1.25rem",
+        fontWeight: 500,
+        lineHeight: 1.6,
+        letterSpacing: "0.0075em",
+        marginTop: 8,
+        marginBottom: 8
+      },
+      "& .MuiExpansionPanel-root:before": {
+        backgroundColor: "unset !important"
+      },
+      "& .MuiFilledInput-underline:before ": {
+        borderBottom: "none !important"
+      }
     },
-    "& p": {
-      fontSize: "1rem",
-      fontWeight: 400,
-      lineHeight: 1.5,
-      letterSpacing: "0.00938em"
+    primaryText: {
+      color: theme.palette.primary.main
     },
-    "& li": {
-      fontSize: "1rem",
-      fontWeight: 400,
-      lineHeight: 1.5,
-      letterSpacing: "0.00938em"
+    dialogTextField: {
+      backgroundColor: "white !important"
     },
-    "& h6": {
-      fontSize: "1.25rem",
-      fontWeight: 500,
-      lineHeight: 1.6,
-      letterSpacing: "0.0075em",
-      marginTop: 8,
-      marginBottom: 8
+    textCenter: {
+      textAlign: "center"
     },
-    "& .MuiExpansionPanel-root:before": {
-      backgroundColor: "unset !important"
+    mt3: {
+      marginTop: theme.spacing(3)
     },
-    "& .MuiFilledInput-underline:before ": {
-      borderBottom: "none !important"
+    mt6: {
+      marginTop: theme.spacing(6)
+    },
+    mb2: {
+      marginBottom: theme.spacing(2)
+    },
+    mb3: {
+      marginBottom: theme.spacing(3)
+    },
+    mb6: {
+      marginBottom: theme.spacing(6)
+    },
+    ml2: {
+      marginLeft: theme.spacing(2)
+    },
+    headerGrid: {
+      position: "relative"
+    },
+    fullHeight: {
+      height: "100vh"
+    },
+    recLogo: {
+      display: "flex",
+      height: 100
+    },
+    blogShortContent: {
+      maxHeight: 600,
+      overflowY: "hidden",
+      textOverflow: "ellipsis"
+    },
+    blogImage: {
+      "& img": {
+        display: "flex",
+        margin: "auto",
+        maxWidth: "100%"
+      }
     }
-  },
-});
+  })
+);
